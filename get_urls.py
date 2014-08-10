6b905fcc50dbd8918b19c49e3f36cd8ea56c301f
oelu <olivierschenk@gmail.com>
2014-08-10 12:13:00 +0200

    added todos

--- get_urls.py
+++ get_urls.py
@@ -18,11 +18,13 @@ def get_website_content(url):
     """
     print "connecting to: " + url
     url = "http://" + url
-    r = requests.get(url)
-    content = r.text
+    response = requests.get(url)
+    content = response.text
     return content
 
 
+# TODO: list only web assets that are fetched by the browser
+# these are: js, img, css files
 def get_urls_in_content(content):
     """
     returns a dict of urls found in content
